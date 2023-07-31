db8dae21e199ea3d098f6481492e84b8478039fb
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-07-31 19:19:50 -0300

    pagina home

--- src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
+++ src/main/java/com/MundoSenaiTDSN/ListaParticipantes/Controller/C_Pessoa.java
@@ -2,13 +2,19 @@ package com.MundoSenaiTDSN.ListaParticipantes.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_Pessoa {
 
     @GetMapping("/")
-    public String landPage(){
+    public String landPage() {
         return "Login/login";
     }
 
+    @PostMapping("/")
+    public String loginPessoa(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha) {
+        return "Home/home";
+    }
 }
